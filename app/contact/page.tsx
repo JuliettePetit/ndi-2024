0b6b95fe7ec9ff334d1ef5637f87f2412ef2cbc0
tdecabrat <tdecabrat@gmail.com>
2024-12-06 00:40:50 +0100

    feat: Added human body component

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import HumanBody from '@/components/ui/humanBody';
+
+export default function Home() {
+    return (
+      <div>
+        <HumanBody width={100} height={100}></HumanBody>
+      </div>
+    );
+  }

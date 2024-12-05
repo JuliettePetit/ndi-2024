c498c6835efd636ae40a06f61c252eb22162f3c8
Juliette Petit <juliette.petit@u-bordeaux.fr>
2024-12-05 22:36:26 +0100

    switch with svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import { Switch } from '@/components/ui/switch';
-import { Earth } from 'lucide-react';
-import { PersonStanding } from 'lucide-react';
 
 export default function Home() {
   return (
     <div>
-      <Earth></Earth>
       <Switch></Switch>
-      <PersonStanding></PersonStanding>
     </div>
   );
 }

46ad236c21736d07e3041411bfa03019a2888425
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2024-12-05 23:35:55 +0100

    wip: update events structure

---
+++ app/update.tsx
@@ -0,0 +1,33 @@
+interface EventDesc {
+  human_desc: string,
+  ocean_desc: string,
+}
+
+interface Event {
+  id: number,
+  name: string,
+  description: EventDesc,
+  option: undefined
+}
+
+const allEvents: Event[] = [];
+
+const resetTimeSecs = 10;
+let curTime: number;
+
+function start() {
+  curTime = Date.now();
+}
+
+function update(): Event | null {
+  if (Date.now() - curTime > resetTimeSecs) {
+    return
+  } else {
+    return null;
+  }
+}
+
+export {
+  start,
+  update
+};

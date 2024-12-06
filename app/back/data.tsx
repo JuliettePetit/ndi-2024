13287772c832fb358da547fbdf3f143396f3c45c
Axel Vendrely Drier <axel.vendrely@enseirb-matmeca.fr>
2024-12-06 06:33:51 +0100

    dernière data

--- app/back/data.tsx
+++ app/back/data.tsx
@@ -149,12 +149,12 @@ export const allEvents: GeoEvent[] = [
   {
     id: 13,
     title: "Lancement de plateformes gazières",
-    description: "Un consortium pétrolier vous propose de développer des plateformes gazières offshore. Cela augmentera les revenus énergétiques de votre région mais pose des risques écologiques. Acceptez-vous ?",
+    description: "Un consortium pétrolier vous propose de développer des plateformes gazières offshore. Cela augmentera les revenus énergétiques de votre région et serait très profitable mais pose des risques écologiques. Acceptez-vous ?",
     consequence: {
       description: "La construction et l’exploitation des plateformes perturbent les écosystèmes marins et libèrent des polluants dans les eaux.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      can_take: {"money" : +1_000_000_000},
+      ocean_changes: {"% substances toxiques": +3},
+      human_changes: {"nb globules blancs par microlitre (de sang)": -1500}
     },
     option: "YesNoChoice"
   },
@@ -163,46 +163,46 @@ export const allEvents: GeoEvent[] = [
     title: "Élimination des déchets des plateformes",
     description: "Après avoir installé des plateformes, vous devez choisir comment gérer les déchets industriels. Optez-vous pour une gestion coûteuse mais écologique, ou une méthode bon marché rejetant des résidus dans l'océan ?",
     consequence: {
-      description: "Les rejets toxiques dans l’océan menacent directement la biodiversité et provoquent une accumulation de contaminants chimiques.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      description: "Les rejets toxiques dans l’océan qui menacaient directement la biodiversité et provoquaient une accumulation de contaminants chimiques.",
+      can_take: {"money" : -550_000_000},
+      ocean_changes: {"% substances toxiques": -1},
+      human_changes: {"nb globules blancs par microlitre (de sang)": +900}
     },
     option: "YesNoChoice"
   },
   {
     id: 15,
     title: "Conflit avec les pêcheurs",
-    description: "Les pêcheurs locaux se plaignent de la baisse des stocks de poissons causée par vos décisions. Pour apaiser la situation, vous pouvez leur verser des subventions (coûteuses) ou ignorer leurs revendications. Que faites-vous ?",
+    description: "Les pêcheurs locaux se plaignent de la baisse des stocks de poissons causée par vos décisions. Pour apaiser la situation, vous pouvez leur verser des subventions (coûteuses) ou ignorer leurs revendications. Laisser les choses passer ?",
     consequence: {
       description: "Les pêcheurs intensifient la surpêche pour compenser, aggravant le déclin des populations marines déjà fragilisées.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      can_take: {"money" : + 55_000_000},
+      ocean_changes: {},
+      human_changes: {}
     },
     option: "YesNoChoice"
   },
   {
     id: 16,
     title: "Stockage de l'énergie gazière",
-    description: "Pour stabiliser l'approvisionnement énergétique, vous pouvez construire des réservoirs souterrains de stockage. Ces infrastructures sont chères et controversées pour leur impact environnemental. Lancez-vous ce projet ?",
+    description: "Pour stabiliser l'approvisionnement énergétique, vous pouvez construire des réservoirs souterrains de stockage. Ces infrastructures sont économe mais controversées pour leur impact environnemental. Lancez-vous ce projet ?",
     consequence: {
       description: "Les fuites de méthane dans l'eau augmentent la saturation en gaz, perturbant les habitats marins sensibles.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      can_take: {"money" : + 500_000_000},
+      ocean_changes: {"% substances toxiques": +4},
+      human_changes: {"nb globules blancs par microlitre (de sang)": -1500}
     },
     option: "YesNoChoice"
   },
   {
     id: 17,
     title: "Fuite de méthane détectée",
-    description: "Des fuites de méthane issues de vos installations sont détectées. Vous pouvez investir dans des réparations coûteuses ou ignorer la situation pour préserver vos finances. Que faites-vous ?",
+    description: "Des fuites de méthane issues de vos installations sont détectées. Vous pouvez investir dans des réparations coûteuses ou ignorer la situation pour préserver vos finances. Je préserve ?",
     consequence: {
       description: "Les émissions de méthane accentuent l’acidification des océans et le réchauffement climatique, perturbant les écosystèmes marins.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      can_take: {"money" : -1_500_000_000},
+      ocean_changes: {"ph": -0.3, "delta température flux marins": +1},
+      human_changes: {"ph sanguin": -0.2, "bpm": -9}
     },
     option: "YesNoChoice"
   },
@@ -212,33 +212,32 @@ export const allEvents: GeoEvent[] = [
     description: "Les installations industrielles augmentent le trafic maritime, vital pour l’économie. Toutefois, cela accroît les risques de collisions, de rejets de carburants et de bruit sous-marin. Laissez-vous cette expansion se poursuivre sans restrictions ?",
     consequence: {
       description: "Le trafic intensif perturbe les mammifères marins, réduit leur capacité de communication et aggrave la pollution par les carburants lourds.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      can_take: {"money" : +250_000_000},
+      ocean_changes: {"delta température flux marins": +0.5, "% substances toxiques": +1.5, "% CO2 absorbable par les mers": -4},
+      human_changes: {"bpm": -3, "nb globules blancs par microlitre (de sang)": -600, "% CO2 filtré par les poumons": -0.8}
     },
     option: "YesNoChoice"
   },
   {
     id: 19,
     title: "Protestation internationale",
-    description: "Face à la dégradation environnementale causée par vos politiques, des ONG dénoncent vos choix. Vous pouvez réduire vos activités industrielles pour restaurer votre image, ou ignorer la pression. Que décidez-vous ?",
+    description: "Face à la dégradation environnementale causée par vos politiques, des ONG dénoncent vos choix. Vous pouvez réduire vos activités industrielles pour restaurer votre image, ou ignorer la pression. J'ignore ?",
     consequence: {
       description: "Ignorer les protestations accélère la destruction des écosystèmes marins et la disparition de nombreuses espèces.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
-    },
+      can_take: {"money" : + 50_000_000},
+      ocean_changes: {"delta température flux marins": 0.1, "% substances toxiques": +1},
+      human_changes: {"nb globules blancs par microlitre (de sang)": -400, "bpm": -3},
     option: "YesNoChoice"
   },
   {
     id: 20,
     title: "Tempête écologique",
-    description: "Une forte tempête frappe vos installations côtières, dispersant des déchets plastiques et pétroliers dans l'océan. Les conséquences sur les écosystèmes marins sont catastrophiques.",
+    description: "Une forte tempête frappe vos installations côtières, dispersant des déchets plastiques et pétroliers dans l'océan. Les conséquences sur les écosystèmes marins sont catastrophiques. Ne pas nettoyer et épurer ?",
     consequence: {
       description: "Les plastiques et les produits chimiques toxiques s'accumulent, endommageant les récifs coralliens et empoisonnant la faune marine.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      can_take: {"money" : + 15_000_000},
+      ocean_changes: {"% substances toxiques": +4},
+      human_changes: {"nb globules blancs par microlitre (de sang)": - 1400} 
     },
     option: "AcceptChoice"
   },
@@ -248,9 +247,9 @@ export const allEvents: GeoEvent[] = [
     description: "La surpêche combinée à la pollution locale provoque un effondrement des populations de poissons dans une zone clé. Cela entraîne une perte massive de biodiversité et un déséquilibre écologique.",
     consequence: {
       description: "La disparition des espèces provoque une réaction en chaîne dans l'écosystème marin, affectant les prédateurs et les récifs coralliens.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      can_take: {"money" : + 25_000_000 },
+      ocean_changes: {"nb globules blancs par microlitre (de sang)": -900},
+      human_changes: {"% substances toxiques": + 2.5}
     },
     option: "AcceptChoice"
   },
@@ -260,9 +259,9 @@ export const allEvents: GeoEvent[] = [
     description: "Les activités industrielles intensives dans la région augmentent la température des eaux côtières. Cela perturbe gravement les habitats naturels des espèces marines locales.",
     consequence: {
       description: "Le réchauffement des eaux accélère la décoloration des coraux et provoque la migration forcée des espèces marines sensibles.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      can_take: {"money" : - 50_000_000},
+      ocean_changes: {"delta température flux marins": +0.9, "% substances toxiques": +2},
+      human_changes: {"bpm": -4, "nb globules blancs par microlitre (de sang)": -450}
     },
     option: "AcceptChoice"
   },
@@ -272,21 +271,21 @@ export const allEvents: GeoEvent[] = [
     description: "Une fuite massive de pétrole se produit à proximité d'une plateforme offshore. Les efforts pour contenir la pollution sont insuffisants, et les conséquences à long terme sont désastreuses.",
     consequence: {
       description: "La marée noire détruit les habitats côtiers et tue des milliers d'animaux marins, des oiseaux aux poissons en passant par les mammifères.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      can_take: {"money" : -500_000_000},
+      ocean_changes: {"ph": -1, "% CO2 absorbable par les mers": -5, "delta température flux marins": +0.9},
+      human_changes: {"bpm": -4, "ph sanguin": -0.3, "% CO2 filtré par les poumons": -1}
     },
     option: "AcceptChoice"
   },
   {
     id: 24,
     title: "Hypoxie marine",
-    description: "L'accumulation de nutriments et de polluants dans l'océan crée une zone morte où l'oxygène disparaît. La vie marine ne peut plus y prospérer.",
+    description: "L'accumulation de nutriments et de polluants dans l'océan crée une zone morte où l'oxygène disparaît. La vie marine ne peut plus y prospérer. Tout est mort.",
     consequence: {
       description: "Les zones mortes s’étendent, éliminant toute vie aquatique et aggravant les déséquilibres dans les écosystèmes environnants.",
-      can_take: {"money" : 5},
-      ocean_changes: ,
-      human_changes: 
+      can_take: {"money" : +5},
+      ocean_changes: {"% substances toxiques": +20},
+      human_changes: {"nb globules blancs par microlitre (de sang)": -5000}
     },
     option: "AcceptChoice"
   }

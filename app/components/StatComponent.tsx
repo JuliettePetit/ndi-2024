a55a3af615051ed83aa4866a97efe7b7f071c596
asolmele <mathis.foussac@gmail.com>
2024-12-06 02:54:51 +0100

    fix

--- app/components/StatComponent.tsx
+++ app/components/StatComponent.tsx
@@ -1,11 +1,11 @@
 import { ColoredSlider } from "./ColoredSlider";
 
-interface Props {
+interface Prop {
   left_color: string;
   right_color: string;
   percent: number;
 }
 
-export default function StatComponent({left_color, right_color, percent}: Props) {
-  return <ColoredSlider disabled hideThumb left_color={left_color} right_color={right_color} defaultValue={[percent]} max={100} step={1}/>
+export default function StatComponent({left_color, right_color, percent}: Prop) {
+  return <ColoredSlider disabled hide_thumb="true" left_color={left_color} right_color={right_color} defaultValue={[percent]} max={100} step={1}/>
 }

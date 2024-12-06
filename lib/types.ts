67d2e2f606eac5b05662d54f9f4a2edd9a34666f
asolmele <ard.foussac@gmail.com>
2024-12-06 04:45:02 +0100

    feat: :raccoon:

--- lib/types.ts
+++ lib/types.ts
@@ -3,19 +3,29 @@ export type TypeOfChoice = 'YesNoChoice' | 'AcceptChoice';
 export type YesNoChoice = 'yes' | 'no';
 export type AcceptChoice = 'ok';
 export type ResponseToChoiceEvent = YesNoChoice | AcceptChoice;
+export type Statistics = "";
 
-export interface Consequence {
-    description: string;
-    apply: Function;
+export interface UpdateResponse {
+  event?: Event,
+  stats: Statistics
+  gameOver: boolean
 }
-
 
 export interface Event {
-    id: number,
-    name: string,
-    description: string,
-    option: TypeOfChoice // what you may send
+  id: number,
+  name: string,
+  description: string,
+  consequence: Consequence,
+  option: TypeOfChoice // what you may send
 }
 
-export interface response {
-    event ?: Event}
+export interface Consequence {
+  description: string;
+  can_take: Stats,
+  ocean_changes: Stats;
+  human_changes: Stats;
+}
+
+export interface Stats {
+  [id: string]: number;
+}

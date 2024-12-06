5e5ce8b459f4b28ff386075d31d8f475f74b7cc9
Juliette Petit <juliette.petit@u-bordeaux.fr>
2024-12-06 02:48:52 +0100

    css && responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary text-secondary`}
       >
         <NavBar />
-          {children}
+        {children}
       </body>
     </html>
   );
--- app/podcasts/page.tsx
+++ app/podcasts/page.tsx
@@ -1,3 +1,6 @@
+import {
+    Card
+} from "@/components/ui/card"
 import DlIcon from "@/components/ui/dlIcon";
 export default function Podcasts() {
     let arr = [
@@ -25,15 +28,19 @@ export default function Podcasts() {
     ];
     return (
 
-        <div className="flex flex-col items-center mt-8">
-            {arr.map((video) => <div className="m-3" key={video.path}>
-                <video controls src={video.path}>
+        <div className="flex flex-col items-center my-8">
+            {arr.map((video) => <Card className="m-3 p-5 relative group bg-primary" key={video.path}>
+                <video className="rounded-lg filter md:hover:brightness-50 " controls src={video.path}>
                 </video>
-                <div className="flex">
-                    <p>{video.title}</p>
-                    <a className="mx-3" href={video.pdf}><DlIcon className="h-5 w-5 stroke-white fill-white"></DlIcon></a>
+                <div className="md:group-hover:flex hidden text-lg absolute left-[25px] top-[10%]">
+                    <p className="ml-4 scroll-m-20 text-md md:text-2xl font-semibold tracking-tight text-secondary">{video.title}</p>
+                    <a className="mx-3" href={video.pdf}><DlIcon className="h-4 w-4 md:h-7 md:w-7 stroke-white fill-white"></DlIcon></a>
                 </div>
-            </div>)}
+                <div className="md:hidden flex text-md mt-4">
+                    <p className="ml-4 scroll-m-20 text-md font-semibold tracking-tight text-secondary">{video.title}</p>
+                    <a className="mx-3" href={video.pdf}><DlIcon className="h-4 w-4 stroke-white fill-white"></DlIcon></a>
+                </div>
+            </Card>)}
         </div>
     )
 }

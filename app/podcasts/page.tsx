21f8aa050394d5de4b3adde1e75844ca8620854f
Juliette Petit <juliette.petit@u-bordeaux.fr>
2024-12-06 02:58:59 +0100

    fix color theme

--- app/podcasts/page.tsx
+++ app/podcasts/page.tsx
@@ -29,15 +29,15 @@ export default function Podcasts() {
     return (
 
         <div className="flex flex-col items-center my-8">
-            {arr.map((video) => <Card className="m-3 p-5 relative group bg-primary" key={video.path}>
+            {arr.map((video) => <Card className="m-3 p-5 relative group " key={video.path}>
                 <video className="rounded-lg filter md:hover:brightness-50 " controls src={video.path}>
                 </video>
                 <div className="md:group-hover:flex hidden text-lg absolute left-[25px] top-[10%]">
-                    <p className="ml-4 scroll-m-20 text-md md:text-2xl font-semibold tracking-tight text-secondary">{video.title}</p>
+                    <p className="ml-4 scroll-m-20 text-md md:text-2xl font-semibold tracking-tight">{video.title}</p>
                     <a className="mx-3" href={video.pdf}><DlIcon className="h-4 w-4 md:h-7 md:w-7 stroke-white fill-white"></DlIcon></a>
                 </div>
                 <div className="md:hidden flex text-md mt-4">
-                    <p className="ml-4 scroll-m-20 text-md font-semibold tracking-tight text-secondary">{video.title}</p>
+                    <p className="ml-4 scroll-m-20 text-md font-semibold tracking-tight ">{video.title}</p>
                     <a className="mx-3" href={video.pdf}><DlIcon className="h-4 w-4 stroke-white fill-white"></DlIcon></a>
                 </div>
             </Card>)}

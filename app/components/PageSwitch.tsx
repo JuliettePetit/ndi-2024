cd63cdec0c0fa99f80a0407f11e0461b8bdfe7de
Juliette Petit <juliette.petit@u-bordeaux.fr>
2024-12-06 06:45:09 +0100

    changed color

--- app/components/PageSwitch.tsx
+++ app/components/PageSwitch.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as SwitchPrimitives from "@radix-ui/react-switch"
-import EarthIcon from '@/components/ui/earthIcon';
+import EarthIcon from "@/components/ui/earthIcon";
 import UserIcon from '@/components/ui/userIcon';
 
 import { cn } from "@/lib/utils"
@@ -13,13 +13,13 @@ const PageSwitch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "group peer inline-flex h-6 w-12 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+      "group peer inline-flex h-6 w-12 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-blue-200 data-[state=unchecked]:bg-red-200",
       className
     )}
     {...props}
     ref={ref}
   >
-    <EarthIcon className="group-data-[state=unchecked]:hidden stroke-white fixed ml-0.5"></EarthIcon>
+    <EarthIcon className="group-data-[state=unchecked]:hidden stroke-background fixed ml-0.5"></EarthIcon>
     <SwitchPrimitives.Thumb
       className={cn(
         "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0"
